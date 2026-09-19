3950e60c90ff8c68a91ce7b1860017b60efd55d3
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-19 19:48:21 +0200

    tableau2.push(tableau1);

--- challange8.js
+++ challange8.js
@@ -4,8 +4,8 @@ const tableau1 = [] ;
 const tableau2 = [] ;
 for ( let i = 0 ; i < elementNum ; i++){
     let element = Number(prompt(`enter les elements ${i +1} : ` ));
-    tableau1.push(element);
-    tableau2.push(tableau1);
+    tableau1.push(element); 
 }
+ tableau2.push(tableau1);
 console.log(`tableau 1 : ${tableau1}`);
-console.log(`tableau 2 : ${tableau1}`);
+console.log(`tableau 2 : ${tableau2}`);
